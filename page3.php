67d9b310b0f4fd179982f164301558eba53ece40
goldingASS <89573687+goldingASS@users.noreply.github.com>
2022-07-20 08:42:32 +0600

    Update page3.php

--- page3.php
+++ page3.php
@@ -1,11 +1,10 @@
 <?php
-$arr=['012','123'];//,'234','345','456','567','678','789'];
+$arr=['012','123','234','345','456','567','678','789'];
 $s=[];
 $obsh=[];
 foreach($arr as $arr1){
 	echo($arr1).'<br>';
 	foreach (range(1,10000) as $number){
-	//$strnumb=strval($number) ;
 	array_push($obsh, $number);
 	if (substr_count($number, $arr1)){
 		array_push($s, $number);
